507d3d48bc08f554d84ca0aeaee87bddc8385a31
javeria-farooq <gettingeducation6@gmail.com>
2024-01-14 20:20:23 +0500

    3rd JS Assignment

---
+++ app.js
@@ -0,0 +1,39 @@
+// let a = +prompt("Enter 1st number")
+// let b = +prompt('Enter 2nd value')
+// let add = a+b
+// let sub = a-b
+// let mul = a*b
+// let div = a/b
+// let mod = a%b
+// document.write(
+//     "<table border='3px'>"+
+//     "<tr>" +
+//     "<th>" + 'Addition' + "</th>"+
+//     "<th>" + 'Subtraction' + "</th>"+
+//     "<th>" + 'multiplication' + "</th>"+
+//     "<th>" + 'Division' + "</th>"+
+//     "<th>" + 'modulus' + "</th>"+
+//     "</tr>"+
+
+//     "<tr>" + 
+//     "<td>" + add + "</td>"+
+//     "<td>" + sub + "</td>"+
+//     "<td>" + mul + "</td>"+
+//     "<td>" + div + "</td>"+
+//     "<td>" + mod + "</td>"+
+//     "</tr>"+
+// "</table>"
+// )
+
+
+// document.write(a + b)
+// document.write(a - b)
+// document.write(a * b)
+// document.write(a / b)
+// document.write(a % b)
+
+
+        //  just for my practice
+// let a = prompt('num')
+// let b = prompt('num2')
+// document.write(a % b)
